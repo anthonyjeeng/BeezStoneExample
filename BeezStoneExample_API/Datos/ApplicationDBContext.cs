dcea9627ca8c6433a6d472a70065e71913348f2c
AnthonyJeeng <somecodes56@gmail.com>
2023-05-26 11:08:00 -0500

    DbContext - Inyeccion de dependencias

---
+++ BeezStoneExample_API/Datos/ApplicationDBContext.cs
@@ -0,0 +1,47 @@
+﻿using BeezStoneExample_API.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeezStoneExample_API.Datos
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa()
+                {
+                    Id = 1,
+                    Nombre = "Villa Real",
+                    Detalle = "Detalle de la villa...",
+                    ImagenUrl = "",
+                    Ocupantes = 5,
+                    MetrosCuadrados = 50,
+                    Tarifa = 200,
+                    Amenidad = "",
+                    FechaCreacion = DateTime.Now,
+                    FechaActualizacion = DateTime.Now,
+                },
+                new Villa()
+                {
+                    Id = 2,
+                    Nombre = "Villa Real",
+                    Detalle = "Detalle de la villa...",
+                    ImagenUrl = "",
+                    Ocupantes = 5,
+                    MetrosCuadrados = 50,
+                    Tarifa = 200,
+                    Amenidad = "",
+                    FechaCreacion = DateTime.Now,
+                    FechaActualizacion = DateTime.Now,
+                }
+            );
+        }
+    }
+}
---
+++ BeezStoneExample_API/Migrations/20230526152740_RellenarTablaVilla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BeezStoneExample_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RellenarTablaVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Detalle", "FechaActualizacion", "FechaCreacion", "ImagenUrl", "MetrosCuadrados", "Nombre", "Ocupantes", "Tarifa" },
+                values: new object[,]
+                {
+                    { 1, "", "Detalle de la villa...", new DateTime(2023, 5, 26, 10, 27, 39, 960, DateTimeKind.Local).AddTicks(7970), new DateTime(2023, 5, 26, 10, 27, 39, 960, DateTimeKind.Local).AddTicks(7958), "", 50, "Villa Real", 5, 200.0 },
+                    { 2, "", "Detalle de la villa...", new DateTime(2023, 5, 26, 10, 27, 39, 960, DateTimeKind.Local).AddTicks(7974), new DateTime(2023, 5, 26, 10, 27, 39, 960, DateTimeKind.Local).AddTicks(7973), "", 50, "Villa Real", 5, 200.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
